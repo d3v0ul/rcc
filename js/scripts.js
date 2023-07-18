13991b1a980821f37a08a923af37355bf854b588
d30ul <zz441766@gmail.com>
2023-07-18 14:41:16 +0300

    18.07

--- js/scripts.js
+++ js/scripts.js
@@ -507,8 +507,6 @@ if (ls.matches) {
 var sync1 = $("#sync1");
 var sync2 = $("#sync2");
 var syncedSecondary = true;
-
-// var hqq = window.matchMedia('all and (max-width: 600px)');
 var hqq = window.matchMedia('all and (max-width: 767px)');
 if (hqq.matches) {
     var slidesPerPage = 4;
@@ -524,7 +522,6 @@ sync1.owlCarousel({
     dots: true,
     loop: true,
     responsiveRefreshRate: 200,
-    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
 }).on('changed.owl.carousel', syncPosition);
 
 sync2
@@ -534,7 +531,7 @@ sync2
     .owlCarousel({
         items: slidesPerPage,
         dots: true,
-        nav: true,
+        nav: false,
         smartSpeed: 200,
         slideSpeed: 500,
         slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
