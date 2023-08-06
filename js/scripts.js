41a864a2661b42b25e0a050e2b8392dc0607e2be
d30ul <zz441766@gmail.com>
2023-08-06 19:38:16 +0300

    6

--- js/scripts.js
+++ js/scripts.js
@@ -151,6 +151,7 @@ $(function() {
                 }
             });
             // круговые прогресбары
+
             let circularProgress = document.querySelector(".circular-progress"),
                 progressValue = document.querySelector(".progress-value");
 
@@ -168,6 +169,7 @@ $(function() {
                     clearInterval(progress);
                 }    
             }, speed);
+
             let circularProgress2 = document.querySelector(".circular-progress2"),
                 progressValue2 = document.querySelector(".progress-value2");
 
@@ -185,6 +187,79 @@ $(function() {
                     clearInterval(progress2);
                 }    
             }, speed2);
+
+            let circularProgress3 = document.querySelector(".circular-progress3"),
+                progressValue3 = document.querySelector(".progress-value3");
+
+            let progressStartValue3 = 0,    
+                progressEndValue3 = 48,    
+                speed3 = 30;
+                
+            let progress3 = setInterval(() => {
+                progressStartValue3++;
+
+                progressValue3.textContent = `${progressStartValue3} %`
+                circularProgress3.style.background = `conic-gradient(#E04141 ${progressStartValue3 * 3.6}deg, #2B2B2B 0deg)`
+
+                if(progressStartValue3 == progressEndValue3){
+                    clearInterval(progress3);
+                }    
+            }, speed3);
+
+            let circularProgress4 = document.querySelector(".circular-progress4"),
+                progressValue4 = document.querySelector(".progress-value4");
+
+            let progressStartValue4 = 0,    
+                progressEndValue4 = 48,    
+                speed4 = 30;
+                
+            let progress4 = setInterval(() => {
+                progressStartValue4++;
+
+                progressValue4.textContent = `${progressStartValue4} %`
+                circularProgress4.style.background = `conic-gradient(#E04141 ${progressStartValue4 * 3.6}deg, #2B2B2B 0deg)`
+
+                if(progressStartValue4 == progressEndValue4){
+                    clearInterval(progress4);
+                }    
+            }, speed4);
+
+            let circularProgress5 = document.querySelector(".circular-progress5"),
+                progressValue5 = document.querySelector(".progress-value5");
+
+            let progressStartValue5 = 0,    
+                progressEndValue5 = 48,    
+                speed5 = 30;
+                
+            let progress5 = setInterval(() => {
+                progressStartValue5++;
+
+                progressValue5.textContent = `${progressStartValue5} %`
+                circularProgress5.style.background = `conic-gradient(#E04141 ${progressStartValue5 * 3.6}deg, #2B2B2B 0deg)`
+
+                if(progressStartValue5 == progressEndValue5){
+                    clearInterval(progress5);
+                }    
+            }, speed5);
+
+            let circularProgress6 = document.querySelector(".circular-progress6"),
+                progressValue6 = document.querySelector(".progress-value6");
+
+            let progressStartValue6 = 0,    
+                progressEndValue6 = 48,    
+                speed6 = 30;
+                
+            let progress6 = setInterval(() => {
+                progressStartValue6++;
+
+                progressValue6.textContent = `${progressStartValue6} %`
+                circularProgress6.style.background = `conic-gradient(#E04141 ${progressStartValue6 * 3.6}deg, #2B2B2B 0deg)`
+
+                if(progressStartValue6 == progressEndValue6){
+                    clearInterval(progress6);
+                }    
+            }, speed6);
+
             //столбцы
             $(".column_progress > div").addClass("fill");
         }
